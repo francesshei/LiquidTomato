3b1bd7253d971ae35cbddb7bad10d40a0c8c35e0
Francesco Sheiban <frances.j.shei@gmail.com>
2025-11-05 17:28:46 +0100

    Initial Commit

---
+++ LiquidTomato/LiquidTomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidTomatoApp.swift
+//  LiquidTomato
+//
+//  Created by Francesco Sheiban on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidTomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
